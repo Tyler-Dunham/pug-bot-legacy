5ecc6b5c001952707287ca28418711a6041a67bc
albionkaferi <albionkaferi02@gmail.com>
2023-05-22 21:32:17 -1000

    added db functionality to commands

--- cogs/profile_commands.py
+++ cogs/profile_commands.py
@@ -7,39 +7,89 @@ import json
 class ProfileCommands(commands.Cog):
     with open('KEYS.json', 'r') as f:
         data = json.load(f)
-        connect_db(data)
+    
 
     # Initialize ProfileCommands
     def __init__(self, client):
         self.client = client
+        self.db = connect_db(self.data)
+        self.players_collection = self.db["players"]
+
 
     # Command for users to create accounts on our database
-    #TODO: Insert these values (tank, dps, support, id, author) to database under profile in profile collection
     @commands.command()
     async def create(self, ctx, tank_elo: int, dps_elo: int, support_elo: int):
-        author = ctx.author
+        author = str(ctx.author)
         author_id = ctx.author.id
-        await ctx.send(f"Tank: {tank_elo} | Dps: {dps_elo} | Support: {support_elo}")
+
+        # Check if document with this name already exists 
+        document = self.players_collection.find_one({"name": author})
+        if document:
+            await ctx.send("You already have an account! Use !view to see your elo.")
+            return
+
+        # Create the player object to be inserted
+        player = {
+            "_id": author_id,
+            "name": author,
+            "tank": tank_elo,
+            "dps": dps_elo,
+            "support": support_elo
+        }
+
+        # Insert the document
+        insert_result = self.players_collection.insert_one(player)
+
+        # Check if the adding failed
+        if not insert_result.acknowledged:
+            ctx.send("Internal Server Error. We cannot create your account at this time.")
+            return
+
+        await ctx.send(f"Successfully created account!\nTank: {tank_elo} | DPS: {dps_elo} | Support: {support_elo}")
+
 
     # Command for users to update accounts on our database
-    #TODO: Update these values in the database under desired profile
     @commands.command()
     async def update(self, ctx, role: str, elo: int):
         author_id = ctx.author.id # For database search purposes, ctx.author works as well but id is unique
-        await ctx.send(f"New {role} sr: {elo}")
+
+        # check that a valid role was given
+        role = role.lower()
+        if (role != "tank" and role != "dps" and role != "support"):
+            await ctx.send("Please enter a valid role (tank, dps, support)")
+            return
+
+        # update the account with the given id to have the given role be the given elo
+        query = {"_id": author_id}
+        update = {"$set": {role: elo}}
+        update_result = self.players_collection.update_one(query, update)
+
+        # check if update failed i.e. no modifications
+        if update_result.modified_count <= 0:
+            ctx.send("Internal Server Error. We cannot update your account at this time.")
+            return
+        
+        await ctx.send(f"Updated {role} elo: {elo}")
+
 
     # Command for users to view accounts on our database
-    #TODO: Search the database for the designated user and display desired info (tank, dps, support elo for now)
     @commands.command()
     async def view(self, ctx, user: str=None):
         # Handles no parameters auto searching for ctx.author
         if user is None:
-            user_to_find = ctx.author
+            user_to_find = str(ctx.author)
         # If a user is specified, search for the specified user
         else:
             user_to_find = user
 
-        await ctx.send(f"Searching for {user_to_find}...")
+        # Look for document with the given name
+        document = self.players_collection.find_one({"name": user_to_find})
+        # Check if no document was found
+        if not document:
+            await ctx.send("We couldn't find an account with that name.")
+            return
+
+        await ctx.send(f"{user_to_find}\nTank: {document['tank']} | DPS: {document['dps']} | Support: {document['support']}")
 
     
 
