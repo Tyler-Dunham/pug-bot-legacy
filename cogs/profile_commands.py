52620c70a99d67d636b5b66ef645a0df89f7c7f2
Tyler <tylerjdunham1@gmail.com>
2023-05-23 16:19:17 -0400

    Added function to check valid elo and default elo

--- cogs/profile_commands.py
+++ cogs/profile_commands.py
@@ -7,13 +7,26 @@ import json
 class ProfileCommands(commands.Cog):
     with open('KEYS.json', 'r') as f:
         data = json.load(f)
-    
 
     # Initialize ProfileCommands
     def __init__(self, client):
         self.client = client
         self.db = connect_db(self.data)
         self.players_collection = self.db["players"]
+
+    def check_valid_elo(self, elos: list):
+        new_elos = []
+
+        # Check if elo is in range
+        for elo in elos:
+            if (elo) not in range(0, 5001):          
+                return new_elos
+            # Finalize elos in new_elos (Default elos if 0)
+            elo = elo if elo != 0 else 2500
+            new_elos.append(elo)
+        
+        return new_elos
+      
 
 
     # Command for users to create accounts on our database
@@ -29,6 +42,15 @@ class ProfileCommands(commands.Cog):
         if document:
             await ctx.send("You already have an account! Use !view to see your elo.")
             return
+
+        # Check if elos are valid
+        elos = [tank_elo, dps_elo, support_elo] # List because check_valid_elo takes a list
+        if len(self.check_valid_elo(elos)) == 0: # A length of 0 means an empty list was returned, meaning there was an invalid elo entered
+            await ctx.send("Invalid elo entered. Command suspended.")
+            return
+        else:
+            # Assign elos (default if 0)
+            tank_elo, dps_elo, support_elo = self.check_valid_elo(elos)
 
         # Create the player object to be inserted
         player = {
@@ -61,6 +83,13 @@ class ProfileCommands(commands.Cog):
         if (role != "tank" and role != "dps" and role != "support"):
             await ctx.send("Please enter a valid role (tank, dps, support)")
             return
+        
+        elos = [elo] #check_valid_elo takes in a list
+        if len(self.check_valid_elo(elos)) == 0: # A length of 0 means an empty list was returned, meaning there was an invalid elo entered
+            await ctx.send("Invalid elo entered. Command suspended.")
+            return
+        else:
+            elo = self.check_valid_elo(elos)[0] # 0th index because check_valid_elo returns a list. In this case it will always be of length 1
 
         # update the account with the given id to have the given role be the given elo
         query = {"_id": author_id}
@@ -69,7 +98,7 @@ class ProfileCommands(commands.Cog):
 
         # check if update failed i.e. no modifications
         if update_result.modified_count <= 0:
-            ctx.send("Internal Server Error. We cannot update your account at this time.")
+            await ctx.send("Internal Server Error. We cannot update your account at this time.")
             return
         
         await ctx.send(f"Updated {role} elo: {elo}")
@@ -89,6 +118,11 @@ class ProfileCommands(commands.Cog):
         document = self.players_collection.find_one({"name": user_to_find})
         # Check if no document was found
         if not document:
+            # User is the author AND no account in the database
+            if user is None:
+                await ctx.send("You don't have an account. Use !create to make one.")
+                return
+            
             await ctx.send("We couldn't find an account with that name.")
             return
 
